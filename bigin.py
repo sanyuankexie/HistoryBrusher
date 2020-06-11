be32bfe430bdd3a8c38393f71825481c2476fb97
PommesPeter <434596665@qq.com>
2020-06-11 15:13:34 +0800

    change the excersise to test dir and fix a bug which occured by score.txt

--- bigin.py
+++ bigin.py
@@ -21,14 +21,20 @@ chooses = ["A", "B", "C", "D"]
 names = ['近代史上篇测试1.csv', '近代史上篇测试2.csv', '近代史中篇测试2.csv', '近代史期中测试1.csv', '近代史期中测试2.csv', '近代史第一次平时练习.csv'
     , '1920215-202001.csv', '1920215-202002.csv', '1920215-下篇1.csv', '1920215-下篇2.csv', '1920215-中篇测试1.csv',
          '1920215-中篇测试2.csv', '1920217-期中测试1.csv']
-data = pd.read_csv(names[0], header=None)
+if os.path.exists("score.txt"):
+    scores = int(read_scores("score.txt"))
+else:
+    file = open("score.txt", "w")
+    file.write("0")
+    scores = int(read_scores("score.txt"))
+data = pd.read_csv('test/' + names[0], header=None)
 for name in names:
-    temp = pd.read_csv(name, header=None)
+    temp = pd.read_csv('test/' + name, header=None)
     data = data.append(temp)
 if is_recode:
     data["recode"] = 0
 data.index = range(data.shape[0])
-scores = int(read_scores("score.txt"))
+
 count = 0
 while True:
     index = np.random.randint(data.shape[0])
